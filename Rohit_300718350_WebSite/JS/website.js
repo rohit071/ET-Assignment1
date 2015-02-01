02baaecade51e6838c524b35c76f4805d484c8c0
AarVee- <rohitinhell@gmail.com>
2015-02-01 12:09:08 -0500

    new web

---
+++ Rohit_300718350_WebSite/JS/website.js
@@ -0,0 +1,78 @@
+﻿var website = angular.module('website', ['ngRoute']);
+
+
+website.service("patientList", function ($http, $q) {
+    var defferred = $q.defer();
+    $http.get('patient.json').then(function (data) {
+        defferred.resolve(data);
+    });
+    this.getPlayers = function () {
+        return defferred.promises;
+    }
+});
+website.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+
+        templateUrl: '../views/home.html',
+    controller : 'mainController'
+    })
+    .when('/about', {
+        
+        templateUrl: '../views/about.html',
+        controller : 'aboutController'
+        
+    })
+     .when('/data', {
+        
+        templateUrl: '../views/patientList.html',
+        controller: 'dataController'
+    })
+      .when('/addPatient', {
+        
+        templateUrl: '../views/PatientDetails.html',
+        controller: 'dataController'
+    })
+
+    .otherwise({
+        redirectTo: '/'
+    });
+
+});
+website.controller('MainCtrl', function ($scope) {
+   
+    
+    
+  
+});
+
+website.controller('mainController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'Home';
+});
+website.controller('aboutController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'About Us';
+});
+website.controller('dataController', function ($scope, $http) {
+    
+    $http.get("patient.json")
+  .success(function (response) { $scope.patients = response; });
+    
+    $scope.addCustomer = function () {
+        $scope.patients.push({ fname : $scope.newPatient.name ,  lname: $scope.newPatient.lname ,  pnumber: $scope.newPatient.number ,  date: $scope.newPatient.date ,  status: $scope.newPatient.status });
+        $scope.newPatient.name = null;
+        $scope.newPatient.lname = null;
+        $scope.newPatient.number = null;
+        $scope.newPatient.date = null;
+    };
+    
+   
+       
+    
+});
+
+
+
+
+     
---
+++ Rohit_300718350_WebSite/server.js
@@ -0,0 +1,28 @@
+﻿
+var http = require('http');
+var express = require('express');
+
+var app = express();
+var port = process.env.port || 1337;
+
+app.use(express.static(__dirname, 'Rohit_300718350_WebSite'));
+
+app.get('/', function (request, response) {
+    response.sendfile("index.html");
+});
+
+
+app.listen(port);
+
+/*
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Simple Web/REST server');
+    console.log('listening on port ' + app.get('port'));
+});
+*/
+
+//app.set('port', process.env.PORT || 3000);
+
+//var server = app.listen(app.get('port'), function () {
+//    debug('Debug Express server listening on port ' + server.address().port);
+//});
